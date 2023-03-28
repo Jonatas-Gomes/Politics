ce10d79836bbc59ae30e7c62ef4e9f5d22842be0
Jônatas Bezerra <jonatas.bezerra@compasso.com.br>
2023-03-28 15:22:04 -0300

    Adding project folder associate

---
+++ associate/src/test/java/br/com/compass/associate/AssociateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compass.associate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssociateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
