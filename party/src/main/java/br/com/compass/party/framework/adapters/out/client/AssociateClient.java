95c9cf8522dac1284f8e5b69595cf8492748d456
Jônatas Bezerra <jonatas.bezerra@compasso.com.br>
2023-03-31 11:53:14 -0300

    Adding FeignClient

---
+++ party/src/main/java/br/com/compass/party/framework/adapters/out/client/AssociateClient.java
@@ -0,0 +1,8 @@
+package br.com.compass.party.framework.adapters.out.client;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+public interface AssociateClient {
+    
+}
