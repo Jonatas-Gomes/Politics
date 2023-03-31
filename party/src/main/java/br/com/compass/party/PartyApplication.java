a384e958c4f465e63667a2518c0dc82abfca1308
Jônatas Bezerra <jonatas.bezerra@compasso.com.br>
2023-03-31 10:56:01 -0300

    Adding Discovery Client

--- party/src/main/java/br/com/compass/party/PartyApplication.java
+++ party/src/main/java/br/com/compass/party/PartyApplication.java
@@ -3,8 +3,10 @@ package br.com.compass.party;
 import jakarta.persistence.Table;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PartyApplication {
 
 	public static void main(String[] args) {
