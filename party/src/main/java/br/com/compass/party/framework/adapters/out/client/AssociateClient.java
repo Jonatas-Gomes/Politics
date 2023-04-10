bfe826c41bf8f89081f12346ade3c69b11581330
Jônatas Bezerra <jonatas.bezerra@compasso.com.br>
2023-04-10 12:41:44 -0300

    Deleted AssociateCliente

--- party/src/main/java/br/com/compass/party/framework/adapters/out/client/AssociateClient.java
+++
@@ -1,8 +0,0 @@
-package br.com.compass.party.framework.adapters.out.client;
-
-import org.springframework.cloud.openfeign.EnableFeignClients;
-
-@EnableFeignClients
-public interface AssociateClient {
-    
-}
