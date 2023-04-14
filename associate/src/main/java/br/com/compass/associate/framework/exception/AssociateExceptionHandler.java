73480bdc9a0775e8f746f2bba0026d6084372ed1
Jônatas Bezerra <jonata.gomes2014@hotmail.com>
2023-04-14 14:56:20 -0300

    fixs imports

--- associate/src/main/java/br/com/compass/associate/framework/exception/AssociateExceptionHandler.java
+++ associate/src/main/java/br/com/compass/associate/framework/exception/AssociateExceptionHandler.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,7 +17,6 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
